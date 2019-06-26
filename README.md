35b77bc5f0c419af9df2d9601bf99b218f50ab66
shefferliao <shefferliao@tencent.com>
2019-06-26 12:33:50 +0800

    执行多进程解析

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 ## 项目介绍
-本项目可以直接将`qmcflac`文件转换成`mp3`文件，暂时没有支持并发，对性能要求高的可以自行扩展
+本项目可以直接将`qmcflac`文件转换成`mp3`文件，目前支持并发
 
 ## 执行方式
 ```bash
-python qmcflac.py -o /tmp/music -i /tmp/music
+python qmcflac.py -o /tmp/mp3_dir -i /tmp/qmcflac_dir -n 5
 ```
 在这里
 ```bash
 -o: 转换mp3输出目录
 -i: 原始qmcflac目录
+-n: 转换文件的进程数，如果不指定，脚本会自动根据转换数量自动决定进程数
 ```
 
 ## 感谢
--- qmcflac.py
+++ qmcflac.py
@@ -8,6 +8,8 @@
 import argparse
 import os
 import shutil
+import multiprocessing
+
 
 root_path = os.path.abspath(os.path.dirname(__file__))
 qmc2flac_tool = os.path.join(root_path, "tools/qmc2flac/decoder")
@@ -15,12 +17,14 @@ flac2mp3_tool = os.path.join(root_path, "tools/flac2mp3/flac2mp3.pl")
 
 
 class Convert(object):
-    def __init__(self, input=None, output=None):
+    def __init__(self, input=None, output=None, num=0):
         self.input = input
         self.output = output if output is not None else input
         self.qmc_files = self.__get_origin_files()
         self.flac_files = []
         self.mp3_files = []
+        self.procs =[]
+        self.num = num if num != 0 else self.__get_proc_num()
 
     def __get_origin_files(self):
         origin_files = []
@@ -39,18 +43,39 @@ class Convert(object):
         print("qmc_to_flac convert finish.")
         return self
 
-    def flac_to_mp3(self):
-        _tmp_dir = "/tmp/flac"
+    def __flac_to_mp3(self, flac_files, _tmp_dir):
         if os.path.exists(_tmp_dir):
             shutil.rmtree(_tmp_dir)
-            os.mkdir(_tmp_dir)
-        for path in self.flac_files:
+        os.mkdir(_tmp_dir)
+        for path in flac_files:
             shutil.move(path, _tmp_dir)
         cmd = "%s %s %s" % (flac2mp3_tool, _tmp_dir, self.output)
         print(cmd)
         os.system(cmd)
         shutil.rmtree(_tmp_dir)
-        print("flac_to_mp3 convert finish.")
+        print("%s flac_to_mp3 convert finish." % _tmp_dir)
+
+    def __chunks(self, files, n):
+        return [files[i:i + n] for i in range(0, len(files), n)]
+
+    def flac_to_mp3(self):
+        if self.num == 0:
+            self.__flac_to_mp3(self.flac_files, "/tmp/flac")
+        else:
+            print("使用线程池，将启动%d个线程" % self.num)
+            groups = self.__chunks(self.flac_files, self.num)
+            for i in range(self.num):
+                p = multiprocessing.Process(target=self.__flac_to_mp3, args=(groups[i], "/tmp/flac-%s" % i))
+                p.start()
+                self.procs.append(p)
+
+            for p in self.procs:
+                p.join()
+
+    def __get_proc_num(self):
+        size = len(self.qmc_files)
+        num = int(size / 5)
+        return num if num <= 8 else 8
 
 
 def main():
@@ -62,8 +87,8 @@ def main():
     parser.add_argument('-o', '--output-dir', type=os.path.abspath,
             help='Directory to output transcoded files to')
 
-    parser.add_argument('-t', '--thread-num', type=os.path.abspath,
-            help='Directory to output transcoded files to')
+    parser.add_argument('-n', '--thread-num', type=int,
+            help='convert thread num')
 
     args = parser.parse_args()
 
@@ -71,8 +96,14 @@ def main():
         input_dir = args.input_dir
     else:
         raise Exception("please input correct input_dir.")
+
+    if args.thread_num is not None:
+        thread_num = args.thread_num
+    else:
+        thread_num = 0
+
     output_dir = args.output_dir
-    convert = Convert(input=input_dir, output=output_dir)
+    convert = Convert(input=input_dir, output=output_dir, num=thread_num)
     convert.qmc_to_flac().flac_to_mp3()
 
 
